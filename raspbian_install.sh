73a06365d0a141cec21f7a421008bc1ed2a136e4
10to7 <elaine.github@10to7.com>
2017-12-27 22:45:24 +0000

    updated the install script to correct issue with raspi-config and added comments to the run.sh

--- raspbian_install.sh
+++ raspbian_install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+echo "Make sure you run this script as sudo"
+echo "sudo ./rasbian_install.sh"
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install libcv-dev python-opencv libopencv-dev libcv2.4
 apt-get -y install libv4l-dev v4l-utils
-raspi-config noint do_camera 1
-raspi-config noint get_config_var gpu_mem_256 /boot/config.txt
+
+# noint doesn't appear to work
+#raspi-config noint do_camera 1
+#raspi-config noint get_config_var gpu_mem_256 /boot/config.txt
+
+# Add the parameters directly to the /boot/config.txt file
+# if you have already run raspi-config you may want to delete
+# the config file and start fresh
+# enable the camera
+echo "start_x=1" >> /boot/config.txt
+# Change the GPU mem to 256 (min would be 128)
+echo "gpu_mem=256" >> /boot/config.txt
+
+echo "Now reboot your rasberry pi"
+echo "sudo reboot"
+echo ""
+echo "Once rebooted go to the location you downloaded the github repository and run"
+echo "./run.sh"
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# We have to sudo the python scrip as we are communicating
+# with the serial port.
+
 sudo python ./follow.py
